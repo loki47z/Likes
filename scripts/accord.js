2da5aab0a43ba6981994d8141bed6d7b12278d80
Thomas Manong'a <thomasmanonga7@gmail.com>
2025-04-10 21:02:37 +0200

    Added Slideshow

--- scripts/accord.js
+++ scripts/accord.js
@@ -24,21 +24,17 @@ function menu(){
     }
 }
 
-let slideIndex = 0;
-showSlides();
-function showSlides(){
-    let i;
-    let slides = document.getElementsByClassName("slides")
-    let dots = document.getElementsByClassName("dot")
-    for(i = 0; i < slides.length; i++){
-        dots[i].style.display = "none"
+var myIndex = 0
+carousel();
+
+function carousel(){
+    var i
+    var x = document.getElementsByClassName("mySlides")
+    for(i = 0; i < x.length; i++){
+        x[i].style.display = "none"
     }
-    slideIndex++;
-    if(slideIndex > slides.length){slideIndex = 1}
-    for(i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace("active", "")
-    }
-    slides[slideIndex-1].style.display = "block"
-    dots[slideIndex-1].className += "active"
-    setTimeout(showSlides, 2000)
+    myIndex++
+    if(myIndex > x.length){myIndex = 1}
+    x[myIndex - 1].style.display = "block"
+    setTimeout(carousel, 2000)
 }
